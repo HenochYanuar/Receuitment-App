c3b827770bb074ee948d6bbc6b9f5a6c1532c508
Henoch <henochyanuar13@gmail.com>
2025-04-30 15:07:37 +0700

    fix(knexfile): add the setting for production environment

--- config/knexfile.js
+++ config/knexfile.js
@@ -1,15 +1,26 @@
 // Update with your config settings.
+require('dotenv').config()
 
-const connection = {
-  client: 'pg',
-  connection: {
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
-      database: process.env.DATABASE_NAME,
-      user: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-  }
-}
+const path = require('path');
+
+const connection = process.env.DATABASE_URL
+  ? {
+      client: 'pg',
+      connection: {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false }
+      }
+    }
+  : {
+      client: 'pg',
+      connection: {
+        host: process.env.DATABASE_HOST,
+        port: process.env.DATABASE_PORT,
+        database: process.env.DATABASE_NAME,
+        user: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+      }
+    }
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
